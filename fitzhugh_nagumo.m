d4af88baffc321121510f70f7da07059e2262094
kyle-sperber-mines2020 <94644876+kyle-sperber-mines2020@users.noreply.github.com>
2024-11-04 14:43:49 -0700

    Model implemented

--- fitzhugh_nagumo.m
+++ fitzhugh_nagumo.m
@@ -1,9 +1,9 @@
-function dxdt = fitzhugh_nagumo(t, V, w, I, params)
+function dxdt = fitzhugh_nagumo(t, y, I, params)
 alpha = params(1);
 epsilon = params(2);
 gamma = params(3);
 
 dxdt = zeros(2, 1);
-dxdt(1) = V .* (V - alpha) .* (1 - V) - w + I(t);
-dxdt(2) = epsilon * (V - gamma * w);
+dxdt(1) = y(1) * (y(1) - alpha) * (1 - y(1)) - y(2) + I(t);
+dxdt(2) = epsilon * (y(1) - gamma * y(2));
 end
