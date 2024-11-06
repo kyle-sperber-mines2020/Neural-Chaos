b38d04db3e1e6d12c983facec7e1b37f0a437018
kyle-sperber-mines2020 <94644876+kyle-sperber-mines2020@users.noreply.github.com>
2024-11-06 14:52:34 -0700

    Plot 3?

--- fitzhugh_nagumo.m
+++ fitzhugh_nagumo.m
@@ -1,9 +1,10 @@
-function dxdt = fitzhugh_nagumo(t, y, I, params)
+function dxdt = fitzhugh_nagumo(t, y, params)
 alpha = params(1);
 epsilon = params(2);
 gamma = params(3);
 
-dxdt = zeros(2, 1);
-dxdt(1) = y(1) * (y(1) - alpha) * (1 - y(1)) - y(2) + I(t);
+dxdt = zeros(3, 1);
+dxdt(1) = y(1) * (y(1) - alpha) * (1 - y(1)) - y(2) + y(3);
 dxdt(2) = epsilon * (y(1) - gamma * y(2));
+dxdt(3) = 0.1 * square(t);
 end
--- fitzhugh_nagumo_solver.m
+++ fitzhugh_nagumo_solver.m
@@ -1,28 +1,38 @@
 %% Define parameters
-alpha = 2;
+alpha = 0.1;
 epsilon = 0.08;
 gamma = 0.1;
 params = [alpha, epsilon, gamma];
 
-I = @(t) sin(t);
-
 t0 = 0;
-tf = 10;
+tf = 100;
 tspan = [t0,tf];
 
-IV = [0;0];
+IV = [0;1;0.2];
 
 %% Solve equation
-[t,v] = ode45(@(t,v) fitzhugh_nagumo(t, v, I, params), tspan, IV);
+[t,v] = ode45(@(t,v) fitzhugh_nagumo(t, v, params), tspan, IV);
 
 
 %% Plot results
-figure;
-plot(t,v,'-'); 
-title('TITLE');
-xlabel('Time, t');
-ylabel('Voltage, V');
-set(gca,'FontSize',16);
-
+close all
 figure
-plot(v(:, 1), v(:, 2))
+plot3(v(:, 1), v(:, 2), v(:, 3), '-');  % Assuming v has at least two columns
+title('Phase Plane');
+xlabel('Voltage, V');
+ylabel('Current, w');
+zlabel('Current, I');
+set(gca, 'FontSize', 16);
+
+% 2D Plot with Voltage against the second variable
+figure;
+plot(v(:, 1), v(:, 2), '-');
+title('Voltage vs Current');
+xlabel('Voltage, V');
+ylabel('Current, w');
+set(gca, 'FontSize', 16);
+
+figure;
+subplot(2, 1, 1);
+plot(t, v, '-')
+legend()
