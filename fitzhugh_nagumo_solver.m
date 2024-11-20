beb3488115f805557f72b38821a405adde4e1953
DrQueue <bselvage@mines.edu>
2024-11-20 14:22:46 -0700

    A

--- fitzhugh_nagumo_solver.m
+++ fitzhugh_nagumo_solver.m
@@ -1,15 +1,19 @@
 %% Define parameters
-alpha = 0.1;
+% alpha = 0.1;
+% epsilon = 0.08;
+% gamma = 0.9;
+alpha = 0.333;
 epsilon = 0.08;
-gamma = 0.9;
+gamma = 0.056;
 params = [alpha, epsilon, gamma];
 
 pulse_duration = 1;
 pulse_magnitude = 0.5; 
-pulse_period = 10; 
+pulse_period = 3; 
 
 I = @(t) (mod(t, pulse_period) < pulse_duration) * pulse_magnitude;
 % I = @(t) 0.5;
+% I = @(t) sin(t)*0.5;
 
 t0 = 0;
 
@@ -20,8 +24,11 @@ IV = [0.5;0];
 
 %% Solve equation
 options = odeset('RelTol', 1e-6, 'AbsTol', 1e-8);
-[t, v] = ode15s(@(t, v) fitzhugh_nagumo(t, v, I, params), tspan, IV, options);
+% [t, v] = ode15s(@(t, v) fitzhugh_nagumo(t, v, I, params), tspan, IV, options);
+[t,v] = ode45(@(t,v) fitzhugh_nagumo(t,v,I,params), tspan, IV, options);
 
+% Remove transient behavior
+IV = [v(end,1), v(end,2)];
 
 %% Plot results
 close all
@@ -39,8 +46,8 @@ grid on
 xlabel('Time,t')
 ylabel('I(t)')
 
-% figure
-% plot(v(:, 1), v(:, 2))
-% grid on 
-% title('Phase Plane')
-% set(gca, 'Box', 'on')
+figure
+plot(v(:, 1), v(:, 2))
+grid on 
+title('Phase Plane')
+set(gca, 'Box', 'on')
